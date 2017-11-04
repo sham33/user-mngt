1b1b81cc1432b8fdcc183e86569b8e6ee699cffc
sham33 <sham.marathe33@gmail.com>
2017-11-04 13:50:59 +0530

    adding jar creation plugin

---
+++ src/main/java/com/example/user/mngt/UserMngtApplication.java
@@ -0,0 +1,12 @@
+package com.example.user.mngt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMngtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMngtApplication.class, args);
+	}
+}
