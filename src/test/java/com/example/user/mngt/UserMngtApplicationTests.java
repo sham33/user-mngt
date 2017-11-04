dd601870e1b6ec4ea6cfc23b88ee433e307c0c37
sham33 <sham.marathe33@gmail.com>
2017-11-04 13:39:56 +0530

    adding missing files

---
+++ src/test/java/com/example/user/mngt/UserMngtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.user.mngt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserMngtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
